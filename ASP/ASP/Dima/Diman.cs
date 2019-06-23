40fea93933eb07c168b47be0d3a55a78faca14b3
N1ght <bushuevdv@mail.ru>
2019-06-23 22:21:06 +0300

    full directory

---
+++ ASP/ASP/Dima/Diman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Dima
+{
+    public class Diman
+    {
+        private String name;
+        private String lastName;
+
+        public Diman()
+        {
+            name = "Dima";
+            lastName = "Bushuev";
+        }
+
+        public List<string> ShowMyName()
+        {
+            var result = new List<string> (){ name, lastName }; 
+            return result;
+        }
+    }
+}
